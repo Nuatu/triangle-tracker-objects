914dfeb194d605a4adf06fb67983333502219b67
Nuatu Tseggai <nuatu@host-123-21.orpopcs.clients.pavlovmedia.com>
2014-07-20 23:38:34 -0700

    created prototype object

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
-var Triangle = function() {
+var Triangle = {
+	initialize: function(a,b,c) {
+		this.side1= a;
+		this.side2= b;
+		this.side3= c;
+		isATriangle: function() {
+			return ((this.side1+this.side2>=this.side3)&&(this.side1+this.side3>=this.side2)&&(this.side2+this.side3>=this.side1))
+	}
+}
 
 
-};
 
 $(document).ready(function(){
-
-$("form#sides").submit(function(event) {
-  event.preventDefault();
-
+	$("form#sides").submit(function(event) {
+  	event.preventDefault();
 
 
-console.log("yup");
-
+	})
 })
 
-})
